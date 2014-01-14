1f6064b491c473d2b957aadadaa05bbc3e2393aa
josdejong <wjosdejong@gmail.com>
2014-01-14 17:29:19 +0100

    Released version 0.3.0

--- updateversion.js
+++ updateversion.js
@@ -1,48 +1,14 @@
-// Update the version numbers and library sizes in index.md
+// Update the version numbers and library sizes in index.html
 
 var fs = require('fs'),
     zlib = require('zlib');
 
-var VIS = 'vis.js',
-    VIS_MIN = 'vis.min.js',
+var VIS_ZIP = './dist/vis.js',
     INDEX = 'index.html';
 
-// get development size
-function developmentSize(callback) {
-  fs.readFile(VIS, function (err, data) {
-    if (!err) {
-      var size = Math.round(data.length / 1024) + ' kB';
-      callback(null, size);
-    }
-    else {
-      callback(err);
-    }
-  });
-}
-
-// get (gzipped) production size
-function productionSize(callback) {
-  fs.readFile(VIS_MIN, function (err, data) {
-    if (!err) {
-      zlib.gzip(data, function (err, data) {
-        if (!err) {
-          var size = Math.round(data.length / 1024) + ' kB';
-          callback(null, size)
-        }
-        else {
-          callback(err);
-        }
-      });
-    }
-    else {
-      callback(err);
-    }
-  });
-}
-
-// get version
+// read version from dist/vis.js
 function version(callback) {
-  fs.readFile(VIS_MIN, function (err, data) {
+  fs.readFile(VIS_ZIP, function (err, data) {
     if (!err) {
       var match = /@version\s*([\w\.-]*)/i.exec(data);
       var version = undefined;
@@ -58,15 +24,10 @@ function version(callback) {
 }
 
 // update version and library sizes in index.md
-function updateVersion(developmentSize, productionSize, version, callback) {
+function updateVersion(version, callback) {
   fs.readFile(INDEX, function (err, data) {
     if (!err) {
       data = String(data);
-      data = data.replace(/<span id="development-size">([\w\s]*)<\/span>/g,
-          '<span id="development-size">' + developmentSize + '</span>');
-
-      data = data.replace(/<span id="production-size">([\w\s]*)<\/span>/g,
-          '<span id="production-size">' + productionSize + '</span>');
 
       data = data.replace(/<span class="version">([\w\.-]*)<\/span>/g,
           '<span class="version">' + version + '</span>');
@@ -79,24 +40,19 @@ function updateVersion(developmentSize, productionSize, version, callback) {
   });
 }
 
-developmentSize(function (err, devSize) {
-  console.log('development size: ' + devSize);
-  productionSize(function (err, prodSize) {
-    console.log('production size: ' + prodSize);
-    version(function (err, version) {
-      console.log('version: ' + version);
-      if (devSize && prodSize && version) {
-        updateVersion(devSize, prodSize, version, function (err, res) {
-          if (err) {
-            console.log(err);
-          }
-          else {
-            console.log('done');
-          }
-        });
+
+version(function (err, version) {
+  console.log('version: ' + version);
+  if (version) {
+    updateVersion(version, function (err, res) {
+      if (err) {
+        console.log(err);
       }
       else {
+        console.log('done');
       }
     });
-  });
+  }
+  else {
+  }
 });
