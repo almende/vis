6b272c1f8f3a53bb7fa626663afcd5fd0da88854
Alex de Mulder <alexdemulder@gmail.com>
2014-04-02 10:30:34 +0200

    rebuild dist

--- dist/vis.js
+++ dist/vis.js
@@ -4,8 +4,8 @@
  *
  * A dynamic, browser-based visualization library.
  *
- * @version 0.7.2-SNAPSHOT
- * @date    2014-04-02
+ * @version @@version
+ * @date    @@date
  *
  * @license
  * Copyright (C) 2011-2014 Almende B.V, http://almende.com
