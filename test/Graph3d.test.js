326a07b9e7a46dd2c283e9374677c09db1f4b2f0
Wim Rijnders <wrijnders@gmail.com>
2017-07-15 11:56:18 +0200

    Proper setup and teardown for jsdom-global

--- test/Graph3d.test.js
+++ test/Graph3d.test.js
@@ -5,11 +5,19 @@ var Graph3d = vis.Graph3d;
 
 
 describe('Graph3d', function () {
-  var jsdom = jsdom_global("<div id='mynetwork'></div>");
-  var container = document.getElementById('mynetwork');
-  assert(container !== null, 'Container div not found');
+
+  before(function() {
+    console.log('before!');
+    this.jsdom_global = jsdom_global(
+      "<div id='mynetwork'></div>",
+      { skipWindowCheck: true}
+    );
+    this.container = document.getElementById('mynetwork');
+  });
 
   it('accepts new option values on defined instance', function () {
+    assert(this.container !== null, 'Container div not found');
+
     var BAR_STYLE = 0;  // from var STYLE in Settings.js
     var DOT_STYLE = 3;  // idem
 
@@ -24,10 +32,16 @@ describe('Graph3d', function () {
       style: 'dot'
     };
 
-    var graph = new vis.Graph3d(container, data, options);
+    var graph = new vis.Graph3d(this.container, data, options);
     assert.equal(graph.style, DOT_STYLE, "Style not set to expected 'dot'");
 
     graph.setOptions({ style: 'bar'});  // Call fails without PR #3255
     assert.equal(graph.style, BAR_STYLE, "Style not set to expected 'bar'");
+  });
+
+
+  after(function() {
+    console.log('after!');
+    this.jsdom_global();
   });
 });
