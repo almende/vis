f1d7a987d612809fd4db377165f9d6a5f142b81e
MacLeod Broad <>
2017-08-08 20:55:29 -0400

    Try the full name

--- magick.sh
+++ magick.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-magick compare -metric RMSE examples/graph2d/01_basic-live.png examples/graph2d/01_basic-local.png NULL:
+imagemagick compare -metric RMSE examples/graph2d/01_basic-live.png examples/graph2d/01_basic-local.png NULL:
