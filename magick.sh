d60425aa13918cfd8ec994e487b058d7b0e9436b
MacLeod Broad <>
2017-08-08 21:00:19 -0400

    Call compare directly

--- magick.sh
+++ magick.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-imagemagick compare -metric RMSE examples/graph2d/01_basic-live.png examples/graph2d/01_basic-local.png NULL:
+compare -metric RMSE examples/graph2d/01_basic-live.png examples/graph2d/01_basic-local.png NULL:
